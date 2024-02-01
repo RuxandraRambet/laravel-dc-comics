9ab9a7b7c154fa75a2160099e02cff37e849f082
RuxandraRambet <ruxandraramb@gmail.com>
2024-02-01 22:31:07 +0100

    feat: seeder

--- database/migrations/2024_02_01_163037_create_comics_table.php
+++ database/migrations/2024_02_01_163037_create_comics_table.php
@@ -15,13 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('title', 100);
             $table->text('description');
-            $table->string('thumb', 255);
-            $table->decimal('price');
+            $table->text('thumb');
+            $table->string('price');
             $table->string('series', 100);
             $table->date('sale_date');
             $table->string('type', 50);
-            $table->string('artists');
-            $table->string('writers');
+    
             $table->timestamps();
         });
     }
