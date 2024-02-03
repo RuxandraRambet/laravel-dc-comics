99a88fe2a47a042f0ac0891dc9cdfb4ec701acab
RuxandraRambet <ruxandraramb@gmail.com>
2024-02-03 16:59:53 +0100

    feat:controller

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -31,3 +31,4 @@ class ComicsTableSeeder extends Seeder
        }
     }
 }
+ 
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel DC Comics</title>
-    @vite( 'resources/js/app.js')
+    <title>DC Comics</title>
 </head>
+
 <body>
-    <div class="container">
-        <h1 class="text-white">Ciao</h1>
-    </div>
+    <h1>Comics</h1>
+    <ul>
+        @foreach ($comics as $comic)
+            <li>{{ $comic->title}}</li>
+        @endforeach
+    </ul>
 </body>
+
 </html>
